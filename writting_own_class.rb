cc2ffd883bbf3591f0136d0b5260a410cf6853c7
Haneczka <hanna.kaptur@gmail.com>
2015-03-27 23:01:54 +0100

    in prison

--- writting_own_class.rb
+++ writting_own_class.rb
@@ -8,7 +8,28 @@ class Human
 	def presentation
 		p @name+" "+@weight+" "+@height
 	end
+	
 end
 		
 hum1 = Human.new("Jola","50","160")
+hum2 = Human.new("Gienek","70","180")
+hum3 = Human.new("Zosia","55","170")
+
 hum1.presentation
+hum2.presentation
+hum3.presentation
+
+def prison(woman1,man1,woman2)
+	cage = [woman1,man1,woman2]
+	p cage
+end
+
+prison(hum1,hum2,hum3)
+
+cage = []
+cage << hum1
+cage << hum2
+
+cage.each do |prisoner|
+	prisoner.presentation
+end
