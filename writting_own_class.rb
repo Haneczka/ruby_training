4822d47e5de42be2fb007e5de1966ff4164662f6
Haneczka <hanna.kaptur@gmail.com>
2015-04-10 22:33:55 +0200

    fix : looking for fatty

--- writting_own_class.rb
+++ writting_own_class.rb
@@ -15,8 +15,25 @@ class Human
 		p "My name is "+@name
 	end
 end
-	
-				
+
+def looking_for_fatty(container)
+	highest_weight = container[0].weight
+	fat_name = container[0].name
+		container.each do |prisoner|
+			#prisoner.introducing
+			if prisoner.weight > highest_weight
+				fat_name = prisoner.name
+				highest_weight = prisoner.weight
+			end	
+			
+			#jesli waga prisonera jest wieksza niz zapamietana
+			# -> zapisz jego wage i imie
+		end
+		#p "The fattest human in this cage is " +fat_name+ "."
+	return fat_name
+end
+
+
 hum1 = Human.new("Jola","50","160")
 hum2 = Human.new("Gienek","70","180")
 hum3 = Human.new("Zosia","55","170")
@@ -27,19 +44,8 @@ cage << hum2
 cage << hum3
 
 
-name_weight = {}
-highest_weight = hum1.weight
-fat_name = hum1.name
-cage.each do |prisoner|
-	prisoner.introducing
-	if prisoner.weight > highest_weight
-		fat_name = prisoner.name
-		highest_weight = prisoner.weight
-	end	
-	
-	#jesli waga prisonera jest wieksza niz zapamietana
-	# -> zapisz jego wage i imie
-end
 
- p "The fattest human in this cage is " +fat_name+ "."
- p "And he weighs "+highest_weight+" kg."
+
+ 
+fatty = looking_for_fatty(cage)
+p fatty
