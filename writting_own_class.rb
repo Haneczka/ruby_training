94a3f6867e91fb920d6f16162128c0b3ad128274
Haneczka <hanna.kaptur@gmail.com>
2015-04-10 21:47:52 +0200

    Who is the fatty in this cage?

--- writting_own_class.rb
+++ writting_own_class.rb
@@ -21,37 +21,25 @@ hum1 = Human.new("Jola","50","160")
 hum2 = Human.new("Gienek","70","180")
 hum3 = Human.new("Zosia","55","170")
 
-p "Whose weight wolud you like to get to know?"
-name_input = gets.chop!
-
 cage = []
 cage << hum1
 cage << hum2
 cage << hum3
 
-#cage.each do |prisoner|
-	#prisoner.presentation
-#end
 
+name_weight = {}
+highest_weight = hum1.weight
+fat_name = hum1.name
 cage.each do |prisoner|
 	prisoner.introducing
-	if prisoner.name == name_input 
-		p "I found #{name_input} he or she is " + prisoner.weight + " kg."
-	end
-end
-puts "\n"
-p "Which of us would you like to know more about? "
-choice = gets.chop!
-
-case choice
-	when "Hania"
-		cage[0].presentation 
-	when "Gien"
-		cage[1].presentation
-	when "Zosia"
-		cage[2].presentation
-	else
-		p "You gave me #{choice} -- I have no idea what to do with that."
+	if prisoner.weight > highest_weight
+		fat_name = prisoner.name
+		highest_weight = prisoner.weight
+	end	
+	
+	#jesli waga prisonera jest wieksza niz zapamietana
+	# -> zapisz jego wage i imie
 end
 
-
+ p "The fattest human in this cage is " +fat_name+ "."
+ p "And he weighs "+highest_weight+" kg."
