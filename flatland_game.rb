616e90495faa463e1150de6b7824c914697b2dae
Haneczka <hanna.kaptur@gmail.com>
2015-04-14 23:20:16 +0200

    Tests passed : 8/10 flatland game

--- flatland_game.rb
+++ flatland_game.rb
@@ -1,16 +1,19 @@
 class Player
   def initialize(x = 0, y = 0)
+	@x = x
+	@y = y
   end
 
   def get_x_position()
-    return nil
+    return @x
   end
 
   def get_y_position()
-    return nil
+    return @y
   end
 
   def move(direction)
     return nil
   end
 end
+
