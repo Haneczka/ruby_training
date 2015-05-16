4e2490b7d5cf2f4ec9fc56ea25ea3e89ac8dbbf5
Haneczka <hanna.kaptur@gmail.com>
2015-05-16 22:13:50 +0200

    test passed moving objects 37/37 flatland game

--- flatland_game.rb
+++ flatland_game.rb
@@ -4,11 +4,11 @@ class ObjectOnMap
     @y = y
     @id = id
   end
-
+  
   def get_x_position()
     return @x
   end
-
+  
   def get_y_position()
     return @y
   end
@@ -38,7 +38,7 @@ class Wall < ObjectOnMap
 end
 
 class Player < ObjectOnMap
-
+  
   def set_x_position(x)
     @x = x 
   end
@@ -46,148 +46,99 @@ class Player < ObjectOnMap
   def set_y_position(y)
     @y = y 
   end
- 
+  
 end
 
 class Game
 	def initialize()
-	end
- 
+  end
+  
   
   def add_object(object)
     #if unless object == nil
     #@objects<<object
-      #if object.is_a?(Wall)
-        #@wall_position = object.get_x_position
-      #end
+    #if object.is_a?(Wall)
+    #@wall_position = object.get_x_position
     #end
-    
+    #end
+ 
     unless object == nil
-     @ids = [] unless @ids
-     @objects = [] unless @objects
+      @ids = [] unless @ids
+      @objects = [] unless @objects
       unless @ids.include?(object.get_id())
         @ids << object.get_id()
         @objects << object
         return true
-      else
+        else
         return false
       end
     end
     
   end
-
+  
   def get_all_objects()
-	@objects = [] unless @objects
+    @objects = [] unless @objects
     return @objects
   end
   
   def get_object(id)
     @objects.select { |p| p.get_id()==id }.first
   end
-
- def move_object(object_id, direction)
+  
+  def move_object(object_id, direction)
     if get_object(object_id) == nil
       return false
-    else
+      else
       #object = get_object(object_id)
       if move(object_id, direction) == false
         return false
       else
         return true
       end
-       #if object.get_x_position = @wall_position
-       # return false
-       #end  
     end
   end
- 
-private
-  #def check_if_it_is_free (object)
-      #taken = {} unless taken
-      #expected = {}
-      #object.get_x_position
-      #object.get_y_position
-        #@objects.each do |obj|
-        #taken = {obj.get_x_position => obj.get_y_position}
-        #end
-        #expected = {object.get_x_position => object.get_y_position}
-        #if taken.detect {|key,val| val == object.get_y_position} == [:object.get_x_position, object.get_y_position]
-          #return false
-        #else
-           #case direction.to_s
-            #when 'right'
-            #object.set_x_position(object.get_x_position+1) 
-            #when 'left'
-            #object.set_x_position(object.get_x_position-1)
-            #when 'up'
-            #object.set_y_position(object.get_y_position+1)
-            #when 'down'
-            #object.set_y_position(object.get_y_position-1)
-           #end
-        #end   
-  #end
   
-  #def speculating (current_location)
-    #case @direction.to_s
-      #when 'right'
-      #@new_x = @current_x+1
-      #when 'left'
-      #@new_x = @current_x-1
-      #when 'up'
-      #@new_y = @current_y+1
-      #when 'down'
-      #@new_y = @current_y-1
-    #end
-    #return @new_x, @new_y
-  #end 
-
+  private
   def move(object_id,direction)
     object = get_object(object_id)
-    @new_x = object.get_x_position
-    @new_y = object.get_y_position
+    new_x = object.get_x_position
+    new_y = object.get_y_position
     
     case direction.to_s
       when 'right'
-      @new_x = object.get_x_position+1
+      new_x = object.get_x_position+1
       when 'left'
-      @new_x = object.get_x_position-1
+      new_x = object.get_x_position-1
       when 'up'
-      @new_y = object.get_y_position+1
+      new_y = object.get_y_position+1
       when 'down'
-      @new_y = object.get_y_position-1
+      new_y = object.get_y_position-1
+      else 
+      new_x = object.get_x_position
+      new_y = object.get_y_position
     end
+    
     
     @objects.each do |obj|
-    
-      if obj.get_x_position == @new_x
-        if obj.get_y_position == @new_y
-         return false
-        else 
-          object.set_x_position(@new_x)
-          object.set_y_position(@new_y)
-        end 
-      else 
-        object.set_x_position(@new_x)
-        object.set_y_position(object.get_y_position)
-      end 
-      
+      if obj.get_x_position == new_x and obj.get_y_position == new_y
+        return false
+      end
     end
+  
+    object.set_x_position(new_x)
+    object.set_y_position(new_y)
+    
+    return true
   end 
   
- 
+  
   def is_already_object_in_database (id)
     if @objects.select { |p| p.get_id()==id }.first != nil
       return true
-    else
+      else
       return false
     end
   end
-
+  
   
 end
-
-
-#zanim poruszę sprawdzam czy proponowana miejscówka jest wolna (iteruję po @objects)
-#jeśli jest wolna wykonuję move na playerze (set_position), jeśli  nie, zwracam false , a położenie playera pozostaje nie zmienione
-
-# przenieść funkcję move do gry, a playerowi wprowadzić set_position 
